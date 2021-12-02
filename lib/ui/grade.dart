af79b0415fbf2c5701868c053061693962e8acc0
sra1kumar-NULL <sravansandeep163@gmail.com>
2021-12-02 20:32:45 +0530

    added logic for all semesters

--- lib/ui/grade.dart
+++ lib/ui/grade.dart
@@ -12,8 +12,8 @@ class GradeCalc extends StatefulWidget {
 
 class _GradeCalcState extends State<GradeCalc> {
   final rollcontroller = TextEditingController();
-  var datalist;
-  var gpa;
+  var sem1,sem2,sem3,sem4,sem5,sem6;
+  var gpa1,gpa2,gpa3,gpa4,gpa5,gpa6;
 
   @override
   Widget build(BuildContext context) {
@@ -37,17 +37,67 @@ class _GradeCalcState extends State<GradeCalc> {
           FutureBuilder(
             builder: (context, snapshot) {
               var showData = json.decode(snapshot.data.toString());
-              datalist=showData;
+              sem1=showData;
               return Text(" ");
             },
             future:
-                DefaultAssetBundle.of(context).loadString("assets/cse/sem6.json"),
+                DefaultAssetBundle.of(context).loadString("assets/cse/sem1.json"),
+          ),
+          FutureBuilder(
+            builder: (context, snapshot) {
+              var showData = json.decode(snapshot.data.toString());
+              sem2=showData;
+              return Text(" ");
+            },
+            future:
+            DefaultAssetBundle.of(context).loadString("assets/cse/sem2.json"),
+          ),
+          FutureBuilder(
+            builder: (context, snapshot) {
+              var showData = json.decode(snapshot.data.toString());
+              sem3=showData;
+              return Text(" ");
+            },
+            future:
+            DefaultAssetBundle.of(context).loadString("assets/cse/sem3.json"),
+          ),
+          FutureBuilder(
+            builder: (context, snapshot) {
+              var showData = json.decode(snapshot.data.toString());
+              sem4=showData;
+              return Text(" ");
+            },
+            future:
+            DefaultAssetBundle.of(context).loadString("assets/cse/sem4.json"),
+          ),
+          FutureBuilder(
+            builder: (context, snapshot) {
+              var showData = json.decode(snapshot.data.toString());
+              sem5=showData;
+              return Text(" ");
+            },
+            future:
+            DefaultAssetBundle.of(context).loadString("assets/cse/sem5.json"),
+          ),
+          FutureBuilder(
+            builder: (context, snapshot) {
+              var showData = json.decode(snapshot.data.toString());
+              sem6=showData;
+              return Text(" ");
+            },
+            future:
+            DefaultAssetBundle.of(context).loadString("assets/cse/sem6.json"),
           ),
 
           FlatButton(
             onPressed: () {
               setState(() {
-                gpa =_calculateGrade(rollcontroller.text);
+                gpa1 =_calculateGrade(rollcontroller.text,sem1);
+                gpa2 =_calculateGrade(rollcontroller.text,sem2);
+                gpa3 =_calculateGrade(rollcontroller.text,sem3);
+                gpa4 =_calculateGrade(rollcontroller.text,sem4);
+                gpa5 =_calculateGrade(rollcontroller.text,sem5);
+                gpa6 =_calculateGrade(rollcontroller.text,sem6);
               });
 
             },
@@ -56,7 +106,22 @@ class _GradeCalcState extends State<GradeCalc> {
             textColor: Colors.white,
           ),
           Text(
-            gpa==null?"":gpa.toStringAsFixed(2)
+            gpa1==null?"":gpa1.toStringAsFixed(2)
+          ),
+          Text(
+              gpa2==null?"":gpa2.toStringAsFixed(2)
+          ),
+          Text(
+              gpa3==null?"":gpa3.toStringAsFixed(2)
+          ),
+          Text(
+              gpa4==null?"":gpa4.toStringAsFixed(2)
+          ),
+          Text(
+              gpa5==null?"":gpa5.toStringAsFixed(2)
+          ),
+          Text(
+              gpa6==null?"":gpa6.toStringAsFixed(2)
           )
 
         ],
@@ -64,75 +129,65 @@ class _GradeCalcState extends State<GradeCalc> {
     );
   }
 
-  _calculateGrade(var check) {
+  _calculateGrade(var check ,var datalist) {
     var index = 0;
     var numer = 0;
     var denom = 0;
+    var subs=0;
     var showData=datalist;
     if(check.substring(0,4)=="1903"){
       index=index+53*9;
-      debugPrint(index.toString());
       while(showData[index]['Htno']!=check){
         index++;
       }
+      // debugPrint(index.toString());
+      // debugPrint(index.toString());
+      // debugPrint(showData[584]['Htno']);
+      // debugPrint(showData[576]['Htno']);
     }
     else{
-      int x=int.parse(check.substring(7));
-      int start=0;
-      int end=showData.length-1;
-      debugPrint(end.toString());
-      while(start<=end){
-        int mid= (start+end)~/2;
-        if(x==int.parse(showData[mid]['Htno'].substring(7))){
-          index=mid;
-          debugPrint(mid.toString());
-          break;
-        }
-        else if(x > int.parse(showData[mid]['Htno'].substring(7))){
-          start=mid+1;
-        }
-        else{
-          end=mid-1;
-        }
-      }
+      int x=int.parse(check.substring(8));
+      index=index+(x-1)*9;
     }
-    debugPrint(check.substring(7));
-    //debugPrint(check.substring(0,4));
-    //debugPrint(index.toString());
     while(showData[index]['Htno']==check){
        if(showData[index]['Grade']=='O'){
             numer+=10*int.parse(showData[index]['Credits']);
-            denom+=10*int.parse(showData[index]['Credits']);
+            denom+=int.parse(showData[index]['Credits']);
        }
        if(showData[index]['Grade']=='S'){
            numer+=9*int.parse(showData[index]['Credits']);
-           denom+=10*int.parse(showData[index]['Credits']);
+           denom+=int.parse(showData[index]['Credits']);
        }
        if(showData[index]['Grade']=='A'){
          numer+=8*int.parse(showData[index]['Credits']);
-         denom+=10*int.parse(showData[index]['Credits']);
+         denom+=int.parse(showData[index]['Credits']);
        }
        if(showData[index]['Grade']=='B'){
          numer+=7*int.parse(showData[index]['Credits']);
-         denom+=10*int.parse(showData[index]['Credits']);
+         denom+=int.parse(showData[index]['Credits']);
        }
        if(showData[index]['Grade']=='C'){
          numer+=6*int.parse(showData[index]['Credits']);
-         denom+=10*int.parse(showData[index]['Credits']);
+         denom+=int.parse(showData[index]['Credits']);
        }
        if(showData[index]['Grade']=='D'){
          numer+=5*int.parse(showData[index]['Credits']);
-         denom+=10*int.parse(showData[index]['Credits']);
+         denom+=int.parse(showData[index]['Credits']);
        }
        if(showData[index]['Grade']=='F'){
          numer+=0;
-         denom+=10*int.parse(showData[index]['Credits']);
+         denom+=int.parse(showData[index]['Credits']);
+       }
+
+       if(index>=showData.length-1){
+         index=showData.length-1;
+         break;
        }
        index+=1;
     }
     debugPrint(numer.toString());
     debugPrint(denom.toString());
-    final result=(numer/denom)*10;
+    final result=numer/denom;
     debugPrint(result.toString());
     return result;
   }
