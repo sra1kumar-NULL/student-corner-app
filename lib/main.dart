ff40a4e46aa32a3bc2962281d6d4777aa269bd6b
sra1kumar-NULL <sravansandeep163@gmail.com>
2022-03-29 21:53:05 +0530

    added a splash screen too

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'package:studentcorner/ui/starthome.dart';
 import 'package:material_color_gen/material_color_gen.dart';
 
@@ -19,7 +20,27 @@ class MyApp extends StatelessWidget {
 
         ),
         debugShowCheckedModeBanner: false,
-        home: const StartScreen());
+        
+        home: SplashScreen(
+          loaderColor: Colors.white54,
+          loadingText: Text("Made with ❤ by Mr.S",
+          style: TextStyle(
+            color: Colors.blueGrey[700],
+            fontSize: 18,
+          ),),
+          title: Text("Student Corner"
+          ,style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+    
+          ),
+          textAlign:TextAlign.center,
+          ),
+          seconds: 5,
+          navigateAfterSeconds: const StartScreen(),
+          backgroundColor: const Color(0xFF5db075),
+        ));
   }
 }
 // sem1  access --- >  https://api.jsonbin.io/b/621dc0c6a703bb67491ef126
